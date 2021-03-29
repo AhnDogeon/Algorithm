5ecbdd9f3d62c5773e69f4b41cf86e047dfcd431
AhnDogeon <qltiqlti@gmail.com>
2021-03-29 22:59:20 +0900

    210329 | 실패율

--- Programmers/kakaoblind_실패율.py
+++ Programmers/kakaoblind_실패율.py
@@ -1,3 +1,4 @@
+# version 1 시간초과 꼭 다시 풀어보기
 def solution(N, stages):
     answer = []
     start = 1
@@ -5,14 +6,17 @@ def solution(N, stages):
     stage_dict = {}
     for i in range(N + 1):
         stage_dict[i + 1] = 0
-    
+
     for x in stages:
         for y in range(x):
             stage_dict[y+1] += 1
-    
+
     for key, value in stage_dict.items():
-        if key <= N:
+        if key <= N and value != 0:
             fail = stages.count(key) / value
+            answer.append([key, fail])
+        elif key <= N and value == 0:
+            fail = 0
             answer.append([key, fail])
             
     answer = sorted(answer, key=lambda x: (-x[1], x[0]))
